21a4c3274ed7ea275d39cf6c784e0a25ace7659e
Garland Kent <galnad@Garlands-MacBook-Pro.local>
2019-09-01 01:16:32 -0600

    Fixed my mistakes in life

--- install-file.sh
+++ install-file.sh
@@ -54,3 +54,104 @@ sed -i 's/!BOTTOKEN!/"$BOT_TOKEN"/g' config.yml
 sed -i 's/!CHANNELS!/{"global": "$CHAT_ID"}/g' config.yml
 sed -i 's/!CONSOLE!/"$CONSOLE_ID"/g' config.yml
 echo -e "Installed Server Config Stuff & DiscordSRV Shit"
+
+# Put anything else above this line!
+# ----------------------------------
+
+start(){
+cd /mnt/server
+echo "Starting Auto Updater Installation..."
+currentvercheck(){
+	if [ -f currentversion.txt ]; then
+		echo "currentversion exists..."
+		cd ../
+		echo "Auto Updater Installation Finished"
+		cd
+	else
+		echo "creating currentversion..."
+		cat << 'EOF' > currentversion.txt
+0
+EOF
+		currentvercheck
+	fi
+}
+currentmc(){
+	if [ -f currentmc.txt ]; then
+		echo "currentmc exists..."
+		currentvercheck
+	else
+		echo "creating currentversion..."
+		cat << 'EOF' > currentmc.txt
+1.14.4
+EOF
+		currentmc
+	fi
+}
+uthingcheck(){
+	if [ -f updatething.sh ]; then
+		echo "updatething exists..."
+		currentmc
+	else
+		echo "creating updatething..."
+		cat << 'EOF' > updatething.sh
+#!/bin/bash
+cd updatescript/
+currentmc=`cat currentmc.txt`
+current=`cat currentversion.txt`
+echo -e "Checking for Server Update..."
+newthing=`curl -s "https://papermc.io/api/v1/paper/${currentmc}" | jq -r '.builds | .latest' 2>&1 | tee latestversion.txt`
+echo -e "Latest Paper is on version ${newthing}"
+startserver(){
+  echo -e "You good on your server/plugin updates my dude."
+  echo -e "Starting server."
+}
+updatecoreplugins(){
+  echo -e "Checking/Updating Core Plugins from Elexi.dev"
+  git checkout master
+  git stash
+  git pull
+  git stash pop --quiet
+  echo -e "Core Plugin Check/Update done."
+  startserver
+}
+comparedemapples(){
+	if [ "${newthing}" -gt "${current}" ]; then
+		echo -e "paper-${newthing}.jar is a new update."
+		echo -e "Updating to paper-${newthing}.jar"
+		wget -nv -nc --content-disposition https://papermc.io/api/v1/paper/${currentmc}/${newthing}/download
+		file="paper-${newthing}.jar"
+		if [ -f "${file}" ]; then
+			echo -e "paper-${newthing}.jar has been downloaded. Renaming some shit..."
+			rm -R ../paper-${current}.jar
+			mv paper-${newthing}.jar ../paper-${newthing}.jar
+			echo -e "${newthing}" > currentversion.txt
+			startserver
+		else
+			echo -e "Error 404: paper-${newthing}.jar could not be found."
+			comparedemapples
+		fi
+	else
+		echo -e "paper-${newthing}.jar is already installed and running."
+		updatecoreplugins
+	fi
+}
+comparedemapples
+EOF
+		chmod +x updatething.sh
+		uthingcheck
+	fi
+}
+dircheck(){
+	if [ -d updatescript ]; then
+		echo "updatescript dir exists..."
+		cd updatescript
+		uthingcheck
+	else
+		echo "creating dir updatescript..."
+		mkdir updatescript
+		dircheck
+	fi
+}
+dircheck
+}
+start
